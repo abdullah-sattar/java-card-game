827f45cf74539f7e0022b9823e7c6a823613e48a
Abdullah Abdulsattar <abdullah_a_s@hotmail.co.uk>
2022-05-21 18:07:57 +0100

    created card and cardgame classes with attributes and constructor

---
+++ src/com/company/Card.java
@@ -0,0 +1,9 @@
+package com.company;
+
+import java.sql.Array;
+
+public class Card {
+    private static String[] suit = {"Heart", "Club", "Diamond", "Spade"};
+    private static String[] symbol = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+    private static int[] value = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+}
---
+++ src/com/company/CardGame.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class CardGame {
+    private ArrayList<Card> deckOfCards = new ArrayList<>();
+
+    public CardGame(ArrayList<Card> deckOfCards) {
+        this.deckOfCards = deckOfCards;
+    }
+}
